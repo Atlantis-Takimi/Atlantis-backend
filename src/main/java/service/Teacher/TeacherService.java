2d5b785c8db0057388985051fb8a059e13ec17cc
Beyza Aktürk <48009801+beyzakturk@users.noreply.github.com>
2022-05-28 14:58:21 +0300

    Add service, repository and controllers for Teacher

---
+++ src/main/java/service/Teacher/TeacherService.java
@@ -0,0 +1,52 @@
+package service.Teacher;
+
+import com.atlantis.model.Teacher.Teacher;
+import com.atlantis.repository.TeacherRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TeacherService {
+    private final TeacherRepository teacherRepository;
+
+    public TeacherService(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+    public List<Teacher> getTeachers(){
+        return teacherRepository.findAll();
+    }
+    public Optional<Teacher> getTeacher(String id){
+        return teacherRepository.findTeacherById(id);
+    }
+    public Optional<Teacher> getTeacherByNumber(String role){
+        return teacherRepository.findTeacherByRole(role);
+    }
+    public void addNewTeacher(Teacher teacher) {
+        Optional<Teacher> TeacherById = teacherRepository.findTeacherById(teacher.getIdTeacher());
+        if(TeacherById.isPresent()){
+            throw new IllegalStateException(
+                    "There is already a Teacher with the given id!");
+        }
+        System.out.println(teacher.getDepartment());
+        teacherRepository.save(teacher);
+        System.out.println(teacher);
+    }
+    public void deleteTeacher(String TeacherId) {
+        boolean exist =teacherRepository.existsById(TeacherId);
+        if(!exist){
+            throw new IllegalStateException(
+                    "Teacher with id="+ TeacherId + " does not exist!");
+        }
+        teacherRepository.deleteById(TeacherId);
+    }
+    @Transactional
+    public void updateTeacher(String TeacherId, Optional<String> name, Optional<String> surname){
+        Teacher teacher = teacherRepository.findTeacherById(TeacherId).orElseThrow(()->
+                new IllegalStateException("Teacher does not exist"));
+        name.ifPresent(teacher::setName);
+        surname.ifPresent(teacher::setSurname);
+    }
+}
