68d6b5ed08f334b2eafeb7d53a7273ad199042c9
ilkeratik <ilkeratik35@gmail.com>
2022-05-30 18:26:15 +0300

    Add StudentDetail and TeacherDetail

---
+++ src/main/java/com/atlantis/model/Student/StudentDetail.java
@@ -0,0 +1,26 @@
+package com.atlantis.model.Student;
+
+import lombok.NonNull;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+public class StudentDetail {
+    @ManyToOne
+    @JoinColumn(name = "idStudent")
+    @Column(name = "idStudent", nullable = false)
+    @NonNull private Student idStudent;
+
+    @Column(name="studentBio")
+    @Type(type="text")
+    private String studentBio;
+
+    @Column(name="studentBioImgPath")
+    @Type(type="text")
+    private String studentBioImgPath;
+
+    @Column(name="studentSocialLink")
+    @Type(type="text")
+    private String studentSocialLink;
+}
---
+++ src/main/java/com/atlantis/model/Teacher/TeacherDetail.java
@@ -0,0 +1,29 @@
+package com.atlantis.model.Teacher;
+
+import com.atlantis.model.Student.Student;
+import lombok.NonNull;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class TeacherDetail {
+    @ManyToOne
+    @JoinColumn(name = "idTeacher")
+    @Column(name = "idTeacher", nullable = false)
+    @NonNull
+    private Teacher idTeacher;
+
+    @Column(name="teacherBio")
+    @Type(type="text")
+    private String teacherBio;
+
+    @Column(name="teacherBioImgPath")
+    @Type(type="text")
+    private String teacherBioImgPath;
+
+    @Column(name="teacherSocialLink")
+    @Type(type="text")
+    private String teacherSocialLink;
+}
