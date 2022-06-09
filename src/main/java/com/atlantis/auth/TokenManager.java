b0f144d4a7c29e17a81a82a32ea52543a3e65408
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-06-09 23:42:26 +0300

    Token Generation for users and administrator

--- src/main/java/com/atlantis/auth/TokenManager.java
+++ src/main/java/com/atlantis/auth/TokenManager.java
@@ -1,4 +1,30 @@
 package com.atlantis.auth;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class TokenManager {
+    private static final String secretKey = "Atlantis2323";
+    private static final int validity = 5 * 60 * 1000 ; // İmzalandıktan 5 dk sonraya kadar geçerli olduğunu varsayarsak..
+    public String generateToken(String username){
+        return Jwts.builder().
+                setSubject(username).
+                setExpiration(new Date(System.currentTimeMillis() + validity)). // Ne zamana kadar geçerli?
+                setIssuer("Admin1"). //Kim İmzaladı,kim yarattı
+                setIssuedAt(new Date(System.currentTimeMillis())). // hangi tarihte oluşturuldu(OLuşturulan an imzalandı)
+                signWith(SignatureAlgorithm.ES256, secretKey).
+                compact();
+    }
+    public boolean tokenValidate(String token){
+
+    }
+    public String getUserFromToken(String token){
+    }
 }
